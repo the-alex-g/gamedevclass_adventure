3bf5004932a5f5fae1c720aee21a26aafe7235e1
Alex G <alex.gestwicki@gmail.com>
2024-08-23 16:51:07 -0400

    Add instantaneous transitions

--- adventure.py
+++ adventure.py
@@ -13,36 +13,44 @@ def load_map(name):
     with open(name + ".map", "r") as map_file:
         node_name = ""
         node_body = ""
+        goto = ""
+        links = []
         for line in map_file.read().splitlines():
             if line.startswith("> node "):
                 if node_name != "":
-                    adventure_map[node_name] = node_body
+                    adventure_map[node_name] = [node_body, links, goto]
                     node_body = ""
+                    links = []
+                    goto = ""
                 node_name = line[7:]
+            elif line.startswith("> linkto "):
+                links.append([
+                    line[9:line.find(" > ")], # link target
+                    line[line.find(" > ") + 3:] # link name
+                ])
+            elif line.startswith("> goto "):
+                goto = line[7:]
             elif line != "":
                 node_body += line + "\n"
-        adventure_map[node_name] = node_body
+        adventure_map[node_name] = [node_body, links, goto]
 
 
 def play_adventure():
     current_node = "START"    
     game_running = True
     while game_running:
-        links = []
-        for line in adventure_map[current_node].splitlines():
-            if line.startswith("> linkto "):
-                link_target = line[9:line.find(" > ")]
-                link_name = line[line.find(" > ") + 3:]
-                links.append([link_target, link_name])
-            else:
-                print("\n" + line)
-        for i in range(0, len(links)):
-            print(f"    {i}) {links[i][1]}")
-        choice = input("Choose an option: ")
-        if choice.isdigit() and int(choice) < len(links):
-            current_node = links[int(choice)][0]
-            if current_node == "END":
-                game_running = False
+        (text, links, goto) = adventure_map[current_node]
+        print(f"\n{text}")
+        if goto != "":
+            current_node = goto
+        else:
+            for i in range(0, len(links)):
+                print(f"    {i}) {links[i][1]}")
+            choice = input("Choose an option: ")
+            if choice.isdigit() and int(choice) < len(links):
+                current_node = links[int(choice)][0]
+        if current_node == "END":
+            game_running = False
 
 
 main()
