1b4be517ca257a0c7f8f3c2bca42122181892add
Alex G <alex.gestwicki@gmail.com>
2024-08-26 12:49:45 -0400

    Add deleting descriptions and links

--- adventure.py
+++ adventure.py
@@ -1,10 +1,45 @@
 class Node:
-    description = ""
+    description = []
     links = {}
     goto = ""
+    replace_with = ""
+    on_exited = ""
+
+    def __init__(self):
+        # Without this, all the nodes share the same dictionaries and arrays
+        self.links = {}
+        self.description = []
 
     def add_link(self, target, text):
-        self.links[text] = target
+        if not text in self.links:
+            self.links[text] = target
+    
+
+    def remove_link(self, text):
+        if text in self.links:
+            del self.links[text]
+    
+    
+    def add_description(self, text):
+        if not text in self.description:
+            self.description.append(text)
+    
+
+    def get_description(self):
+        return separate(self.description, " ")
+
+
+    def remove_description(self, text):
+        new_description = []
+        for old_description in self.description:
+            if not old_description == text:
+                new_description.append(old_description)
+        self.description = new_description
+    
+
+    def exit(self):
+        # Runs code when this node is exited
+        eval_commands(self.on_exited, self)
 
 
 def main():
@@ -58,22 +93,39 @@ def separate(array, spacer):
     return string
 
 
-def _advfunc_node(root, name, *rest):
+def _advfunc_node(adventure, name, *rest):
+    # creates a node and adds it to the adventure
     node = Node()
     eval_commands(separate(rest, " "), node)
-    root[name] = node
+    adventure[name] = node
 
 
-def _advfunc_text(root, *text):
-    root.description = separate(text, " ")
+def _advfunc_text(node, *text):
+    node.add_description(separate(text, " "))
 
 
-def _advfunc_link(root, *foo):
-    root.add_link(foo[0], separate(foo[1:], " "))
+def _advfunc_link(node, link_target, *link_text):
+    node.add_link(link_target, separate(link_text, " "))
 
 
-def _advfunc_goto(root, target):
-    root.goto = target
+def _advfunc_goto(node, target):
+    node.goto = target
+
+
+def _advfunc_on_exit(node, *contents):
+    node.on_exited = separate(contents, " ")
+
+
+def _advfunc_become(node, new_name):
+    node.replace_with = new_name
+
+
+def _advfunc_rm_link(node, *link_text):
+    node.remove_link(separate(link_text, " "))
+
+
+def _advfunc_rm_text(node, *text):
+    node.remove_description(separate(text, " "))
 
 
 def play(adventure):
@@ -81,11 +133,14 @@ def play(adventure):
     game_running = True
     while game_running:
         node = adventure[current_node]
-        print(f"\n{node.description}")
+        while node.replace_with != "":
+            node = adventure[node.replace_with]
+        print(f"\n{node.get_description()}")
         if node.goto:
             current_node = node.goto
         else:
             current_node = node.links[get_input_from_menu(sorted(node.links))]
+        node.exit()
         if current_node == "END":
             game_running = False
 
