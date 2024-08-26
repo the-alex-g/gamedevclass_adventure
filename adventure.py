ac0c4a7974c95aa8a4599bec4c8df52339920cd3
Alex G <alex.gestwicki@gmail.com>
2024-08-26 12:21:18 -0400

    Add something akin to proper programming functionality

--- adventure.py
+++ adventure.py
@@ -1,7 +1,15 @@
+class Node:
+    description = ""
+    links = {}
+    goto = ""
+
+    def add_link(self, target, text):
+        self.links[text] = target
+
 
 def main():
     menu_options = {
-        "Load an adventure":lambda: play(load_adventure(input("Enter the file name: ")))
+        "Load an adventure":lambda: play(load_adventure("dungeon"))#input("Enter the file name: "))
     }
     menu_options[get_input_from_menu(["Load an adventure"])]()
 
@@ -10,40 +18,74 @@ def load_adventure(name):
     adventure = {}
 
     with open(name + ".adv", "r") as map_file:
-        node_name = ""
-        node_body = ""
-        goto = ""
-        links = {}
-        for line in map_file.read().splitlines():
-            if line.startswith("> node "):
-                if node_name != "":
-                    adventure[node_name] = [node_body, links, goto]
-                    node_body = ""
-                    links = {}
-                    goto = ""
-                node_name = line[7:]
-            elif line.startswith("> linkto "):
-                link_name = line[line.find(" > ") + 3:]
-                link_target = line[9:line.find(" > ")]
-                links[link_name] = link_target
-            elif line.startswith("> goto "):
-                goto = line[7:]
-            elif line != "":
-                node_body += line + "\n"
-        adventure[node_name] = [node_body, links, goto]
+        eval_commands(map_file.read(), adventure)
+
     return adventure
+
+
+def eval_commands(string, root):
+    args = []
+    arg_text = ""
+    indents = 0
+    for char in string:
+        if char in " ]\n":
+            if arg_text != "":
+                args.append(arg_text)
+                arg_text = ""
+        elif indents > 0:
+            arg_text += char
+        if char == "[":
+            indents += 1
+        elif char == "]":
+            indents -= 1
+            arg_text += "]"
+            if indents == 0:
+                run_command(args[0], root, args[1:])
+                args = []
+                arg_text = ""
+
+
+def run_command(name, root, args):
+    eval(f'_advfunc_{name}(root, "{separate(args, '", "')}")')
+
+
+def separate(array, spacer):
+    string = ""
+    for item in array:
+        if string != "":
+            string += spacer
+        string += str(item)
+    return string
+
+
+def _advfunc_node(root, name, *rest):
+    node = Node()
+    eval_commands(separate(rest, " "), node)
+    root[name] = node
+
+
+def _advfunc_text(root, *text):
+    root.description = separate(text, " ")
+
+
+def _advfunc_link(root, *foo):
+    root.add_link(foo[0], separate(foo[1:], " "))
+
+
+def _advfunc_goto(root, target):
+    root.goto = target
 
 
 def play(adventure):
     current_node = "START"    
     game_running = True
     while game_running:
-        (text, links, goto) = adventure[current_node]
-        print(f"\n{text}")
-        if goto == "":
-            current_node = links[get_input_from_menu(sorted(links))]
+        node = adventure[current_node]
+        print(f"\n{node.description}")
+        if node.goto:
+            current_node = node.goto
         else:
-            current_node = goto
+            current_node = node.links[get_input_from_menu(sorted(node.links))]
         if current_node == "END":
             game_running = False
 
